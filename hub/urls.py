0f9f79daad74553b10827f5faeedca824d06549f
Wasim Afser <wasim.afser@gmail.com>
2023-04-07 17:32:42 +0530

    fix drf view import

--- hub/urls.py
+++ hub/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.authtoken import drf_views
+from rest_framework.authtoken import views as drf_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
