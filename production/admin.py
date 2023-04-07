5b37f98d345ae110fb45f24d878785fe0c839b30
Wasim Afser <wasim.afser@gmail.com>
2023-04-07 22:44:43 +0530

    production: admin site changes

--- production/admin.py
+++ production/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 from production import models
 
 # Register your models here.
-admin.site.register(models.Project)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'state')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+    list_filter = ('state',)
+
+
+admin.site.register(models.Project, admin_class=ProjectAdmin)
+admin.site.register(models.ProjectState)
+admin.site.register(models.ProjectType)
