7129607de2cb17756959e677bf4d3dcd720e1b6b
Wasim Afser <wasim.afser@gmail.com>
2023-04-08 20:46:33 +0530

    include tasks models

--- production/admin.py
+++ production/admin.py
@@ -15,3 +15,17 @@ class ProjectAdmin(admin.ModelAdmin):
 admin.site.register(models.Project, admin_class=ProjectAdmin)
 admin.site.register(models.ProjectState)
 admin.site.register(models.ProjectType)
+admin.site.register(models.ProjectPriority)
+admin.site.register(models.TaskPriority)
+admin.site.register(models.TaskStatus)
+admin.site.register(models.TaskType)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'project', 'type', 'priority')
+    search_fields = ('title', 'description')
+    ordering = ('priority',)
+    list_filter = ('status', 'project', 'type', 'priority')
+
+
+admin.site.register(models.Task, admin_class=TaskAdmin)
--- production/models.py
+++ production/models.py
@@ -35,6 +35,22 @@ class ProjectType(models.Model):
         return self.name
 
 
+class ProjectPriority(models.Model):
+    """
+    Defines priorities for a project.
+
+    name: name of the priority.
+    order: step-up order of the priority.
+    description: description of the priority.
+    """
+    name = models.CharField(max_length=200)
+    order = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Project(models.Model):
     """
     Contains details about a project.
@@ -44,12 +60,118 @@ class Project(models.Model):
     extras: extra information about the project.
     type: type of the project.
     state: state of the project.
+    priority: priority of the project.
+    start_date: start date of the project.
+    end_date: end date of the project.
+    created_at: date of creation.
+    updated_at: date of last update.
     """
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     extras = models.JSONField(default=dict)
     type = models.ForeignKey(ProjectType, on_delete=models.PROTECT, null=True)
     state = models.ForeignKey(ProjectState, on_delete=models.SET_NULL, null=True)
+    priority = models.ForeignKey(ProjectPriority, on_delete=models.SET_NULL, null=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.name
+
+
+class TaskPriority(models.Model):
+    """
+    Defines priorities for a task.
+
+    name: name of the priority.
+    order: step-up order of the priority.
+    description: description of the priority.
+    """
+    name = models.CharField(max_length=200)
+    order = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskStatus(models.Model):
+    """
+    Defines statuses for a task.
+
+    name: name of the status.
+    order: step-up order of the status.
+    description: description of the status.
+    """
+    name = models.CharField(max_length=200)
+    order = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskType(models.Model):
+    """
+    Defines types for a task.
+
+    name: name of the type.
+    priority: prioirty multiplier of the type.
+    description: description of the type.
+    """
+    name = models.CharField(max_length=200)
+    priority = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    """
+    Contains details about a task.
+
+    title: title of the task.
+    description: description of the task.
+    extras: extra information about the task.
+    project: project the task belongs to.
+    priority: priority of the task.
+    status: status of the task.
+    type: type of the task.
+    created_by: user who created the task.
+    assigned_to: user who is assigned to the task.
+    assign_date: date when the task is assigned.
+    start_date: date when the task starts.
+    end_date: date when the task ends.
+    expected_start_date: date when the task is expected to start.
+    expected_end_date: date when the task is expected to end.
+    created_at: date of creation.
+    updated_at: date of last update.
+    extras: extra information about the task.
+    """
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    extras = models.JSONField(default=dict)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    priority = models.ForeignKey(TaskPriority, on_delete=models.SET_NULL, null=True)
+    status = models.ForeignKey(TaskStatus, on_delete=models.SET_NULL, null=True)
+    type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
+    created_by = models.ForeignKey('auth.User', on_delete=models.PROTECT)
+    assigned_to = models.ForeignKey(
+        'auth.User', on_delete=models.PROTECT, related_name='assigned_to'
+    )
+    assign_date = models.DateTimeField(null=True, blank=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    expected_start_date = models.DateTimeField(null=True, blank=True)
+    expected_end_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    expected_completion_duration = models.DurationField(null=True, blank=True)
+    completion_duration = models.DurationField(null=True, blank=True)
+    extras = models.JSONField(default=dict)
+
+    def __str__(self) -> str:
+        return self.title
